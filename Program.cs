dea6d5c6677d6d02fc0998162ec0bd8aa8418d8a
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-04-22 17:34:55 +0300

    update read.me

--- Program.cs
+++ Program.cs
@@ -4,9 +4,7 @@
     {
         public class MinimumInsertionStepsMakeStringPalindrome
         {
-            private int[,] dp;
-
-            private int DFS(string s1, string s2, int m, int n)
+            private int DFS(string s1, string s2, int m, int n, int[,] dp)
             {
                 if (m == 0 || n == 0)
                 {
@@ -16,13 +14,13 @@
                 {
                     return dp[m, n];
                 }
-                if (s1[m] == s2[n])
+                if (s1[m - 1] == s2[n - 1])
                 {
-                    dp[m, n] = 1 + DFS(s1, s2, m - 1, n - 1);
+                    dp[m, n] = 1 + DFS(s1, s2, m - 1, n - 1, dp);
                 }
                 else
                 {
-                    dp[m, n] = Math.Max(DFS(s1, s2, m - 1, n), DFS(s1, s2, m, n - 1));
+                    dp[m, n] = Math.Max(DFS(s1, s2, m - 1, n, dp), DFS(s1, s2, m, n - 1, dp));
                 }
                 return dp[m, n];
             }
@@ -30,13 +28,16 @@
             public int MinInsertions(string s)
             {
                 int n = s.Length;
-                string reverseStr = new string(s.Reverse().ToArray());
-                dp = new int[n + 1, n + 1];
+                string reverseStr = new(s.Reverse().ToArray());
+                int[,] dp = new int[n + 1, n + 1];
                 for (int i = 0; i <= n; ++i)
                 {
-                    dp[i, i] = -1;
+                    for(int j = 0; j <= n; ++j)
+                    {
+                        dp[i, j] = -1;
+                    }
                 }
-                return n - DFS(s, reverseStr, n, n);
+                return n - DFS(s, reverseStr, n, n, dp);
             }
         }
 
